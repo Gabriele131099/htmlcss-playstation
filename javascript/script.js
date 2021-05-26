32305f7b6ca7851e704325abf9d7ad13771bc3b7
gabriele131099 <gabrisanna131099@gmail.com>
2021-05-27 00:06:46 +0200

    struttura

--- javascript/script.js
+++ javascript/script.js
@@ -1,2 +1,19 @@
 alert("compra la tua playstation 5");
 console.log("Pippo Topolino");
+
+var a = 10;
+var b = 20;
+var paperino = b - a;
+
+document.getElementById('sottrazione').innerHTML= paperino;
+
+
+
+
+if (a>b) {
+
+    
+    
+} else {
+    
+}
